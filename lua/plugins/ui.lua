9d14c95c7798347bcb36ed900f8cb64c8d90632d
Jose Vega <josevega234@gmail.com>
2024-03-22 15:14:02 +0100

    noice fixes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,57 +2,27 @@ return {
   {
     "folke/noice.nvim",
     dependencies = { "rcarriga/nvim-notify", "MunifTanjim/nui.nvim" },
-    config = function()
-      local focused = true
-      require("noice").setup {
-        lsp = {
-          signature = {
-            enabled = false,
-          },
+    opts = {
+      lsp = {
+        signature = {
+          enabled = false,
         },
-        commands = {
-          all = {
-            -- options for the message history that you get with `:Noice`
-            view = "split",
-            opts = { enter = true, format = "details" },
-            filter = {},
-          },
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
-        routes = {
-          {
-            filter = {
-              event = "notify",
-              find = "No information available",
-            },
-            opts = { skip = true },
-          },
-          {
-            filter = {
-              cond = function() return not focused end,
-            },
-            view = "notify_send",
-            opts = { stop = false },
-          },
-        },
-        presets = {
-          lsp_doc_border = true,
-        },
-      }
-
-      vim.api.nvim_create_autocmd("FocusGained", {
-        callback = function() focused = true end,
-      })
-      vim.api.nvim_create_autocmd("FocusLost", {
-        callback = function() focused = false end,
-      })
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "markdown",
-        callback = function(event)
-          vim.schedule(function() require("noice.text.markdown").keys(event.buf) end)
-        end,
-      })
-    end,
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
   },
   {
     "zbirenbaum/neodim",
