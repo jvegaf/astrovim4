36936065d2062cf02c262839378693810659d0bd
Jose Vega <josevega234@gmail.com>
2024-08-30 17:46:17 +0200

    micropython

---
+++ lua/plugins/micropython.lua
@@ -0,0 +1,6 @@
+return {
+  event = "VeryLazy",
+  "jim-at-jibba/micropython.nvim",
+  dependencies = { "akinsho/toggleterm.nvim", "stevearc/dressing.nvim" },
+  config = function() vim.keymap.set("n", "<leader>mr", require("micropython_nvim").run) end,
+}
