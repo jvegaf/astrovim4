019e4a08b724888e6be951be35c57c65d3d423af
Jose Vega <josevega234@gmail.com>
2025-10-30 02:57:53 +0100

    copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -119,29 +119,64 @@ local chat = {
   },
   keys = {
     {
-      "<Leader>ao",
-      function() require("CopilotChat").open() end,
-      mode = { "n", "v" },
+      "<leader>ap",
+      ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
+      mode = "x",
+      desc = "CopilotChat - Prompt actions",
+    },
+    -- Code related commands
+    { "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+    { "<leader>at", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+    { "<leader>ar", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
+    { "<leader>aR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
+    { "<leader>an", "<cmd>CopilotChatBetterNamings<cr>", desc = "CopilotChat - Better Naming" },
+    -- Chat with Copilot in visual mode
+    {
+      "<leader>av",
+      ":CopilotChatVisual",
+      mode = "x",
+      desc = "CopilotChat - Open in vertical split",
     },
     {
-      "<Leader>ad",
-      function()
-        local sticky = {
-          "#buffers",
-          "#gitdiff",
-        }
-        if vim.bo.filetype == "python" then table.insert(sticky, "/PythonExpert") end
-
-        require("CopilotChat").ask(
-          "Please update the comments, docstrings, and unit tests related to my changed code",
-          {
-            sticky = sticky,
-          }
-        )
-      end,
-      mode = { "n", "v" },
-      desc = "Update tests, docstrings based on new changes",
+      "<leader>ax",
+      ":CopilotChatInline<cr>",
+      mode = "x",
+      desc = "CopilotChat - Inline chat",
     },
+    -- Custom input for CopilotChat
+    {
+      "<leader>ai",
+      function()
+        local input = vim.fn.input "Ask Copilot: "
+        if input ~= "" then vim.cmd("CopilotChat " .. input) end
+      end,
+      desc = "CopilotChat - Ask input",
+    },
+    -- Generate commit message based on the git diff
+    {
+      "<leader>am",
+      "<cmd>CopilotChatCommit<cr>",
+      desc = "CopilotChat - Generate commit message for all changes",
+    },
+    -- Quick chat with Copilot
+    {
+      "<leader>aq",
+      function()
+        local input = vim.fn.input "Quick Chat: "
+        if input ~= "" then vim.cmd("CopilotChatBuffer " .. input) end
+      end,
+      desc = "CopilotChat - Quick chat",
+    },
+    -- Debug
+    { "<leader>ad", "<cmd>CopilotChatDebugInfo<cr>", desc = "CopilotChat - Debug Info" },
+    -- Fix the issue with diagnostic
+    { "<leader>af", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
+    -- Clear buffer and chat history
+    { "<leader>al", "<cmd>CopilotChatReset<cr>", desc = "CopilotChat - Clear buffer and chat history" },
+    -- Toggle Copilot Chat Vsplit
+    { "<leader>av", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
+    -- Copilot Chat Models
+    { "<leader>a?", "<cmd>CopilotChatModels<cr>", desc = "CopilotChat - Select Models" },
   },
   cmd = {
     "CopilotChat",
